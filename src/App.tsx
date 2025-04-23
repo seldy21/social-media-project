403002bffdd75bf9992eedf231b86b017001516d
seldy21 <seldy@naver.com>
2025-04-23 21:15:55 +0900

    Add routing with react-router-dom and remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/posts" element={<div>post list</div>} />
+      <Route path="/posts/:id" element={<div>post detail</div>} />
+      <Route path="/posts/new" element={<div>post new</div>} />
+      <Route path="/posts/edit/:id" element={<div>post edit</div>} />
+      <Route path="/profile" element={<div>profile</div>} />
+      <Route path="/profile/edit" element={<div>profile edit</div>} />
+      <Route path="/notification" element={<div>notification</div>} />
+      <Route path="/search" element={<div>search</div>} />
+      <Route path="/login" element={<div>login</div>} />
+      <Route path="/signup" element={<div>signup</div>} />
+      <Route path="*" element={<Navigate replace to="/"/>} />
+    </Routes>
   );
 }
 
